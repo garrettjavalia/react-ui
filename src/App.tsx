d20c617268aff6b9e0c05cb26dac822b980e6eb4
garrettjavalia <45995618+garrettjavalia@users.noreply.github.com>
2021-09-05 15:00:40 +0900

    keep working on issue list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import IssueList from './IssueList';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Anglular / Angular-cli
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Switch>
+          <Route path="/issue-details">
+            {/*<Issue-Details/>*/}
+          </Route>
+          <Route path="/">
+            <IssueList />
+          </Route>
+        </Switch>
+
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/IssueList.tsx
@@ -0,0 +1,60 @@
+import './App.css';
+import { makeStyles } from '@material-ui/core';
+import { Fragment, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  issueItemStyle:{
+
+  },
+}));
+
+function IssueList() {
+
+  const classes = useStyles();
+
+  const [issueList, setIssueListState] = useState([]);
+  const [lastUpdatedPage, setLastUpdatedPageState] = useState(-1);
+
+  function fetchMoreData(){
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      setIssueListState(issueList.concat(Array.from({ length: 20 })));
+    }, 1500);
+
+  };
+
+  return (
+
+    <Fragment>
+
+      <InfiniteScroll
+        dataLength={issueList.length}
+        next={fetchMoreData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        {issueList.map((i, index) => (
+          <div className={classes.issueItemStyle} key={index}>
+            div - #{index}
+          </div>
+        ))}
+      </InfiniteScroll>
+
+    </Fragment>
+
+  );
+}
+
+export default IssueList;
